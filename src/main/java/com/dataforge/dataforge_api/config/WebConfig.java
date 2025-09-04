7eb14ee426d45c08f30d283ea6a372115d097e58
João Santos <joao75559@gmail.com>
2025-09-03 23:37:55 -0300

    url

--- src/main/java/com/dataforge/dataforge_api/config/WebConfig.java
+++ src/main/java/com/dataforge/dataforge_api/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permite CORS em todos os endpoints
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000") // Adicione as portas dos ambientes de desenvolvimento
+                .allowedOrigins("https://dataaforge.netlify.app/") // Adicione as portas dos ambientes de desenvolvimento
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
